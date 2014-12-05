7e47a392763fd3aecf9d68e8706c5653443df553
Salo <christopher.saloman@esa.int>
2014-12-05 13:54:11 +0100

    add recommendations of pep8

--- replazer.py
+++ replazer.py
@@ -3,6 +3,7 @@
 import sys
 import re
 
+
 def main():
     if not_2_arguments_provided():
         print_usage()
@@ -10,7 +11,7 @@ def main():
 
     template_file = sys.argv[1]
     output_file = sys.argv[2]
-    
+
     template_lines = get_lines_of(template_file)
     template_variables = get_variables_from(template_lines)
     print_user_message(template_variables)
@@ -19,14 +20,17 @@ def main():
 
     write_lines(rendered_lines, output_file)
 
+
 def not_2_arguments_provided():
     if not len(sys.argv) == 3:
         return True
     return False
 
+
 def get_lines_of(template_file):
     with open(template_file) as f:
         return f.readlines()
+
 
 def get_variables_from(template_lines):
     ret = set()
@@ -37,10 +41,13 @@ def get_variables_from(template_lines):
 
     return ret
 
+
 def print_user_message(template_variables):
-    print('I found ' + str(len(template_variables)) + ' variables in the template:')
+    print('I found ' + str(len(template_variables)) +
+          ' variables in the template:')
     print(list(template_variables))
     print('Please give them a meaning:')
+
 
 def ask_user_for_fillings(template_variables):
     filled_variables = {}
@@ -54,15 +61,19 @@ def ask_user_for_fillings(template_variables):
         filled_variables[variable] = meaning
     return filled_variables
 
+
 def render_lines_with(template_fillings, template_lines):
     output = []
     for line in template_lines:
         matches = re.finditer(r'{{([^}]+)}}', line)
         for match in matches:
-            current_filling = get_filling_for(match.group(1).strip(), match.group(0), template_fillings)
+            current_filling = get_filling_for(match.group(1).strip(),
+                                              match.group(0),
+                                              template_fillings)
             line = line.replace(match.group(0), current_filling)
         output.append(line)
     return output
+
 
 # Code smell envy --> own dict class
 def get_filling_for(variable_name, variable_representation, template_fillings):
@@ -73,17 +84,22 @@ def get_filling_for(variable_name, variable_representation, template_fillings):
         raise VariableNotDefinedError(variable_representation)
     return ret
 
+
 def write_lines(rendered_lines, output_file):
     with open(output_file, 'w') as f:
         f.writelines(rendered_lines)
 
+
 def print_usage():
     print("Usage: " + sys.argv[0] + " template_name output_name")
 
+
 class VariableNotDefinedError(KeyError):
     def __init__(self, variable_representation):
-        self.message = 'Variable ' + variable_representation + ' was found, but not defined!'
+        self.message = ('Variable ' + variable_representation +
+                        ' was found, but not defined!')
         self.variable = variable_representation
+
 
 if __name__ == "__main__":
     main()
--- test_replazer.py
+++ test_replazer.py
@@ -1,73 +1,76 @@
 #!/usr/bin/env python
 
 import unittest
-from fill_template import get_variables_from, render_lines_with, VariableNotDefinedError
+from fill_template import (get_variables_from,
+                           render_lines_with,
+                           VariableNotDefinedError)
+
 
 class Test(unittest.TestCase):
     def testGetsNoVariablesFromListWithNone(self):
-        my_list = [
-                    "Hello World",
-                    "I am a text",
-                    "with no variables"
-                  ]
+        my_list = ["Hello World",
+                   "I am a text",
+                   "with no variables",
+                   ]
         output = get_variables_from(my_list)
         expected = set()
-        self.assertSetEqual(expected, output);
+        self.assertSetEqual(expected, output)
 
     def testTextIsUnalteredWhenNoVariablesAreIncludedAfterRendering(self):
-        my_list = [
-                    "Hello World",
-                    "I am a text",
-                    "with no variables"
-                  ]
+        my_list = ["Hello World",
+                   "I am a text",
+                   "with no variables",
+                   ]
         output = render_lines_with(set(), my_list)
         expected = my_list
         self.assertListEqual(output, expected)
 
     def testGetsVariablesFromList(self):
-        my_list = [
-                    "foo bar\n",
-                    "{{VAR}}",
-                    "fooo {{VAR2}}",
-                    "safa {{VAR}} {{VAR3}} {{   VAR2   }}",
-                  ]
+        my_list = ["foo bar\n",
+                   "{{VAR}}",
+                   "fooo {{VAR2}}",
+                   "safa {{VAR}} {{VAR3}} {{   VAR2   }}",
+                   ]
         output = get_variables_from(my_list)
         expected = set(['VAR', 'VAR2', 'VAR3'])
-        self.assertSetEqual(output, expected) 
+        self.assertSetEqual(output, expected)
 
     def testInsertsVariables(self):
-        my_list = [
-                    "foo bar\n",
-                    "{{VAR}}",
-                    "fooo {{VAR2}}",
-                    "safa {{VAR}} {{VAR3}} {{   VAR2   }}",
-                  ]
-
-        output = render_lines_with({'VAR': 'VAR', 'VAR2' : 'VAR2', 'VAR3' : 'VAR3'}, my_list)
-        expected = [
-                    "foo bar\n",
+        my_list = ["foo bar\n",
+                   "{{VAR}}",
+                   "fooo {{VAR2}}",
+                   "safa {{VAR}} {{VAR3}} {{   VAR2   }}",
+                   ]
+        output = render_lines_with({'VAR': 'VAR',
+                                    'VAR2': 'VAR2',
+                                    'VAR3': 'VAR3',
+                                    },
+                                   my_list)
+        expected = ["foo bar\n",
                     "VAR",
                     "fooo VAR2",
                     "safa VAR VAR3 VAR2",
-                  ]
+                    ]
         self.assertListEqual(output, expected)
 
     def testRaisesExceptionWhenAskedToRenderButNotAllVariablesAreDefined(self):
-        my_list = [
-                    "foo bar\n",
-                    "{{VAR}}",
-                    "fooo {{VAR2}}",
-                    "safa {{VAR}} {{VAR3}} {{   VAR2   }}",
-                  ]
+        my_list = ["foo bar\n",
+                   "{{VAR}}",
+                   "fooo {{VAR2}}",
+                   "safa {{VAR}} {{VAR3}} {{   VAR2   }}",
+                   ]
         try:
-            render_lines_with({'VAR2' : 'VAR2', 'VAR3' : 'VAR3'}, my_list)
+            render_lines_with({'VAR2': 'VAR2',
+                               'VAR3': 'VAR3'},
+                              my_list)
         except VariableNotDefinedError as e:
-            self.assertEqual(e.message, 'Variable {{VAR}} was found, but not defined!')
+            self.assertEqual(e.message,
+                             'Variable {{VAR}} was found, but not defined!')
             self.assertEqual(e.variable, '{{VAR}}')
             return
         self.fail('VariableNotDefinedError not thrown')
-        
+
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
